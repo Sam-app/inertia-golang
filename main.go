6165dbb70d99d3245110881aad4e9a731c75f46f
Samuel <samuel.mebrahtu@atos.net>
2023-04-11 12:23:05 +0100

    add: edit handler

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"example.com/inertia-golang/models"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -14,9 +16,21 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", title, "body")
 }
-
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := models.LoadPage(title)
+	if err != nil {
+		p = &models.Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
 func main() {
 	http.HandleFunc("/view/", viewHandler)
-	// http.HandleFunc("/", handler)
+	http.HandleFunc("/edit/", editHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
